3c761a1ab254d3fd9d345fe786d5247149404a3b
Niran Babalola <niran@niran.org>
2017-01-17 16:56:18 -0600

    Clear seed phrase input after confirming it

--- src/handlers/handleVerifySeed.js
+++ src/handlers/handleVerifySeed.js
@@ -8,9 +8,16 @@ export default function handleVerifySeed(event) {
       if (seed.toLowerCase() == event.target.value.toLowerCase()) {
         const confirmButton = el('#view-campaign-contribute-wallet-confirm a.confirm');
         confirmButton.removeAttribute('disabled');
+
         // Remove the seed from the DOM.
-        const seedEl = el('#view-campaign-contribute-wallet-seed .seed');
-        seedEl.innerHTML = '...';
+        const seedDisplayEl = el('#view-campaign-contribute-wallet-seed .seed');
+        seedDisplayEl.innerHTML = '...';
+
+        // When the user confirms the seed, clear the input box.
+        const seedInputEl = event.target;
+        confirmButton.addEventListener('click', () => {
+          seedInputEl.value = '';
+        });
       }
     });
 }
