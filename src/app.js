47fb5bc157f0a73efa3f5fb2983376785d60d460
Niran Babalola <niran@niran.org>
2017-01-20 21:49:57 -0600

    Remove zoom on Macs to fix the slider bug. Fixes #164

--- src/app.js
+++ src/app.js
@@ -20,11 +20,6 @@ function loadApp(loadAppEvent) {
   // window warnign message
   window.onunload = window.onbeforeunload = handleConfirmOnPageExit;
 
-  // fix mac focus
-  if (navigator.userAgent.toLowerCase().indexOf('mac') !== -1) {
-    document.body.style.zoom = '0.8';
-  }
-
   // setup the web3 provider
   if (loadAppEvent.bypassWeb3Provider !== true) {
     setupWeb3Provider();
